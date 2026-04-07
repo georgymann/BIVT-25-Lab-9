228e12f24d3efce09ecefa8698175ef5c279ea26
Georgy <georgy_mann@mail.ru>
2026-04-07 20:42:55 +0300

    task1 complete

--- Lab9/Blue/Blue.cs
+++ Lab9/Blue/Blue.cs
@@ -1,3 +1,14 @@
 ﻿namespace Lab9.Blue
 {
+    public abstract class Blue
+    {
+        public string Input { get; protected set; }
+        protected Blue(string input) => Input = input;
+        public abstract void Review();
+        public virtual void ChangeText(string text)
+        {
+            Input = text;
+            Review();
+        }
+    }
 }
---
+++ Lab9/Blue/Task1.cs
@@ -0,0 +1,57 @@
+namespace Lab9.Blue
+{
+    public class Task1 : Blue
+    {
+        public Task1(string input) : base(input) {}
+        public string[] Output { get; protected set; }
+        public override void Review()
+        {
+            var words = Input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string current = "";
+            int count = 0;
+            foreach (var i in words)
+            {
+                if (current.Length == 0)
+                {
+                    current += i;
+                }
+                else if (current.Length != 0 && current.Length + 1 + i.Length <= 50)
+                {
+                    current += " " + i;
+                }
+                else
+                {
+                    count++;
+                    current = i;
+                }
+            }
+            if (current.Length > 0) count++;
+            Output = new string[count];
+            int ind = 0;
+            current = "";
+            foreach (var i in words)
+            {
+                if (current.Length == 0)
+                {
+                    current = i;
+                }
+                else if (current.Length + 1 + i.Length <= 50)
+                {
+                    current += " " + i;
+                }
+                else
+                {
+                    Output[ind++] = current;
+                    current = i;
+                }
+            }
+
+            if (current.Length > 0) Output[ind] = current;
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, Output);
+        }
+        
+    }
+}
